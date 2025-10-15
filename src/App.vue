65996c69285a591757e9fa51f6f12ab702d0916d
Zikora <zikorapascal@gmail.com>
2025-10-15 21:46:56 +0100

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,13 @@
-<template></template>
+<script setup>
+import navbar from './components/navbar.vue'
+import Hero from './components/Hero.vue'
+import HomeCards from './components/HomeCards.vue'
+import JobListings from './components/JobListings.vue'
+</script>
+
+<template>
+  <navbar />
+  <Hero />
+  <HomeCards />
+  <JobListings />
+</template>
